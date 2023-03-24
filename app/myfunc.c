b57d07df9558f75f566309167c8d5ae2e4306b5d
withoutwarningg <124793432+withoutwarningg@users.noreply.github.com>
2023-03-24 20:55:03 +0300

    Update myfunc.c
    delete myfunc

--- app/myfunc.c
+++ app/myfunc.c
@@ -3,12 +3,6 @@
 #include <stdlib.h>
 #include "myfunc.h"
 
-int myfunc(int b) {
-    char *buffer = malloc(sizeof(char) * 1000);
-    buffer [0] = b + 4;
-    // здесь должен ругаться sonarcloud, т.к. утечка памяти
-    return buffer[0];
-}
 
 int val;
 
